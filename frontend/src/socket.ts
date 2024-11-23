85917d5e333829d6a7d6d951da751271f7817ea4
Mike Medvedev <mmedvedev20@gmail.com>
2024-11-23 18:39:56 -0500

    hardcoding prod url

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,5 @@
 // import { v4 as uuid4 } from "uuid";
 import { uniqueNamesGenerator, Config, names } from "unique-names-generator";
-
 const config: Config = {
   dictionaries: [names],
 };
@@ -11,7 +10,9 @@ class WebSocketManager {
   public clientId: string = uniqueNamesGenerator(config);
   private constructor() {
     const characterName: string = uniqueNamesGenerator(config);
-    this.socket = new WebSocket(`ws://localhost:8000/ws/${characterName}`);
+    this.socket = new WebSocket(
+      `wss://immortal-journal-server.azurewebsites.net/ws/${characterName}`
+    );
   }
   public static getInstance(): WebSocketManager {
     if (!WebSocketManager._instance) {
